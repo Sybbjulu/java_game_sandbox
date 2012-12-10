161e2e37610db9702e98535cba01f2d46b23b14e
a-out <richbone227@gmail.com>
2012-12-10 01:37:29 -0500

    fixed grid dimensions to fill up 640x480 window

--- src/minecraft2d/World.java
+++ src/minecraft2d/World.java
@@ -3,7 +3,7 @@ package minecraft2d;
 public class World {
 
 	public static final int BLOCK_SIZE = 32;
-	public static final int BLOCKS_WIDTH = 20;
-	public static final int BLOCKS_HEIGHT = 20;
+	public static final int BLOCKS_WIDTH = 21;
+	public static final int BLOCKS_HEIGHT = 16;
 	
 }
