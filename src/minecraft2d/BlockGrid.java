affba5a9e8edca98333aff96983054f0c4a66a4a
a-out <richbone227@gmail.com>
2012-12-10 00:29:08 -0500

    added BlockGrid

---
+++ src/minecraft2d/BlockGrid.java
@@ -0,0 +1,35 @@
+package minecraft2d;
+
+import static minecraft2d.World.*;
+
+public class BlockGrid {
+
+	private Block[][] blocks = new Block[BLOCKS_WIDTH][BLOCKS_HEIGHT];
+	
+	public BlockGrid() {
+		
+		// initialize 2d block array
+		for (int x = 0; x < BLOCKS_WIDTH - 1; x++) {
+			for (int y = 0; y < BLOCKS_HEIGHT - 1; y++ ) {
+				blocks[x][y] = new Block(BlockType.AIR, x * BLOCK_SIZE, y * BLOCK_SIZE);
+			}
+		}
+	}
+	
+	public void setAt(int x, int y, BlockType b) {
+		blocks[x][y] = new Block(b, x * BLOCK_SIZE, y * BLOCK_SIZE);
+	}
+	
+	public Block getAt(int x, int y) {
+		return blocks[x][y];
+	}
+	
+	public void draw() {		
+		for (int x = 0; x < BLOCKS_WIDTH - 1; x++) {
+			for (int y = 0; y < BLOCKS_HEIGHT - 1; y++ ) {
+				blocks[x][y].draw();
+			}
+		}
+	}
+	
+}
